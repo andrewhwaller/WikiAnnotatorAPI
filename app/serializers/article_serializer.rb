6be4291ca2d8983385b6ccb3946342b7777330ae
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2020-07-26 20:10:03 -0500

    Update article_serializer.rb

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,6 +1,6 @@
 class ArticleSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :title, :extract, :pageid, :created_at, :updated_at
+    attributes :title, :extract, :pageid, :created_at, :updated_at, :id
     belongs_to :user
     cache_options enabled: true, cache_length: 12.hours
   end
